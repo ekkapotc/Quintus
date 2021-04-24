493710b0f5908b2f8befa1fe07b0e9d6316e0424
ekkapotc <37437364+ekkapotc@users.noreply.github.com>
2021-04-24 14:26:13 +0700

    restructured local repo

---
+++ src/printReport.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import matplotlib
+from pylab import title, figure, xlabel, ylabel, xticks, bar, legend, axis, savefig
+from fpdf import FPDF
+import os
+
+def set_dll_search_path():
+   # Python 3.8 no longer searches for DLLs in PATH, so we have to add
+   # everything in PATH manually. Note that unlike PATH add_dll_directory
+   # has no defined order, so if there are two cairo DLLs in PATH we
+   # might get a random one.
+   if os.name != "nt" or not hasattr(os, "add_dll_directory"):
+       return
+   for p in os.environ.get("PATH", "").split(os.pathsep):
+       try:
+           os.add_dll_directory(p)
+       except OSError:
+           pass
+
+set_dll_search_path()
+
+
+df = pd.DataFrame()
+df['Question'] = ["Q1", "Q2", "Q3", "Q4"]
+df['Charles'] = [3, 4, 5, 3]
+df['Mike'] = [3, 3, 4, 4]
+
+title("Professor Criss's Ratings by Users")
+xlabel('Question Number')
+ylabel('Score')
+
+c = [2.0, 4.0, 6.0, 8.0]
+m = [x - 0.5 for x in c]
+
+xticks(c, df['Question'])
+
+bar(m, df['Mike'], width=0.5, color="#91eb87", label="Mike")
+bar(c, df['Charles'], width=0.5, color="#eb879c", label="Charles")
+
+legend()
+axis([0, 10, 0, 8])
+savefig('../img/barchart.png')
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_xy(0, 0)
+pdf.set_font('arial', 'B', 12)
+pdf.cell(60)
+pdf.cell(75, 10, "A Tabular and Graphical Report of Professor Criss's Ratings by Users Charles and Mike", 0, 2, 'C')
+pdf.cell(90, 10, " ", 0, 2, 'C')
+pdf.cell(-40)
+pdf.cell(50, 10, 'Question', 1, 0, 'C')
+pdf.cell(40, 10, 'Charles', 1, 0, 'C')
+pdf.cell(40, 10, 'Mike', 1, 2, 'C')
+pdf.cell(-90)
+pdf.set_font('arial', '', 12)
+for i in range(0, len(df)):
+    pdf.cell(50, 10, '%s' % (df['Question'].iloc[i]), 1, 0, 'C')
+    pdf.cell(40, 10, '%s' % (str(df.Mike.iloc[i])), 1, 0, 'C')
+    pdf.cell(40, 10, '%s' % (str(df.Charles.iloc[i])), 1, 2, 'C')
+    pdf.cell(-90)
+pdf.cell(90, 10, " ", 0, 2, 'C')
+pdf.cell(-30)
+pdf.image('../img/barchart.png', x = None, y = None, w = 0, h = 0, type = '', link = '')
+pdf.output('../report/report.pdf', 'F')
---
+++ src/randmCSV.py
@@ -0,0 +1,65 @@
+import random
+import datetime
+import pandas as pd
+import numpy as np
+
+def random_date():
+    currentTime = datetime.datetime.now()
+    nextTime = currentTime + datetime.timedelta(minutes=random.randrange(1,60))
+    return nextTime  
+
+records = int(input('Enter the number of records to generate: '))
+
+print('Generating {0} records...\n'.format(records))
+
+columns =('LightID','Timestamp','V1','V2','V3','V4','V5','Distance','Color','Result')
+
+possible_colors   =['W', 'R', 'B', 'G']
+possible_outcomes =['Pass', 'Fail']
+
+timestamps = []
+lightids   = []
+v1s = []
+v2s = []
+v3s = []
+v4s = []
+v5s = []
+distances = []
+colors = []
+outcomes = []
+
+#randomly generate records
+for i in range(0, records):
+    lightids.append(i+1)
+    timestamps.append(random_date()) 
+    v1s.append(random.uniform(40,50))
+    v2s.append(random.uniform(40,50))
+    v3s.append(random.uniform(40,50))
+    v4s.append(random.uniform(40,50))
+    v5s.append(random.uniform(40,50))
+    distances.append(random.uniform(0.50,1.80))
+    colors.append(np.random.choice(possible_colors,p=[0.25,0.25,0.25,0.25]))
+    outcomes.append(np.random.choice(possible_outcomes,p=[0.95,0.05]))
+
+#assemble columns 
+data = {    columns[0]:lightids,
+            columns[1]:timestamps,
+            columns[2]:v1s,
+            columns[3]:v2s,
+            columns[4]:v3s,
+            columns[5]:v4s,
+            columns[6]:v5s,
+            columns[7]:distances,
+            columns[8]:colors,
+            columns[9]:outcomes
+       }
+
+#construct data frame
+df = pd.DataFrame(data,columns=columns)
+pd.options.display.max_rows = records
+
+#set 'LightID' as index
+df.set_index(['LightID'],inplace=True)
+
+#save data frame tp .csv file
+df.to_csv('../data/m_data.csv',float_format='%.2f')
